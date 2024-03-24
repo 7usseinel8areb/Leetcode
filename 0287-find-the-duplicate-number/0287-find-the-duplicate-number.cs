096f0d0bfc1c332af4717cd533ace6de8ed4261c
Hussein Elghareb <111694991+7usseinel8areb@users.noreply.github.com>
2024-03-25 01:14:26 +0200

    Time: 272 ms (54.94%), Space: 74.9 MB (21.43%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
@@ -1,8 +1,22 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
-        return nums.GroupBy(x => x)
-                   .Where(g => g.Count() > 1)
-                   .Select(g => g.Key)
-                   .FirstOrDefault();
+        Dictionary<int, int> numCount = new Dictionary<int, int>();
+        
+        // Count occurrences of each number
+        foreach(int num in nums){
+            if(!numCount.ContainsKey(num)){
+                numCount[num] = 1;
+            } else {
+                numCount[num]++;
+            }
+        }
+        
+        foreach(var i in numCount){
+            if(i.Value > 1){
+                return i.Key;
+            }
+        }
+        
+        return -1;
     }
 }
