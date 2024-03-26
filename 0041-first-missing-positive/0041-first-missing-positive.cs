cbd2f0a60f1a766e542e99c8f68cc6e1210b082b
Hussein Elghareb <111694991+7usseinel8areb@users.noreply.github.com>
2024-03-26 18:54:21 +0200

    Time: 168 ms (94.44%), Space: 58.4 MB (44.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cs
+++ 0041-first-missing-positive/0041-first-missing-positive.cs
@@ -4,11 +4,10 @@ public class Solution
     {
         Array.Sort(nums);
         int index = Array.FindIndex(nums, i => i > 0);
-        if(nums[nums.Length-1]<1)return 1;
-        if (nums[index] > 1)
-        {
+        if(nums[nums.Length-1]<1)
             return 1;
-        }
+        if (nums[index] > 1)
+            return 1;
         for (int i = index; i < nums.Length-1; i++)
         {
             if (nums[i] + 1 < nums[i + 1]) 
