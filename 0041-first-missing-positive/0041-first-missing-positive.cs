fc7d9f467a57bb99af1899876bf73ffb4fab3661
Hussein Elghareb <111694991+7usseinel8areb@users.noreply.github.com>
2024-03-26 18:56:09 +0200

    Time: 181 ms (62.25%), Space: 58.2 MB (59.48%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cs
+++ 0041-first-missing-positive/0041-first-missing-positive.cs
@@ -3,16 +3,16 @@ public class Solution
     public int FirstMissingPositive(int[] nums)
     {
         Array.Sort(nums);
-        int index = Array.FindIndex(nums, i => i > 0);
-        if(nums[nums.Length-1]<1)
-            return 1;
-        if (nums[index] > 1)
-            return 1;
-        for (int i = index; i < nums.Length-1; i++)
+        int smallestPositive = 1;
+
+        foreach (var num in nums)
         {
-            if (nums[i] + 1 < nums[i + 1]) 
-                return nums[i]+1;
+            if (num == smallestPositive)
+                smallestPositive++;
+            else if (num > smallestPositive)
+                return smallestPositive;
         }
-        return nums[nums.Length-1] + 1;
+
+        return smallestPositive;
     }
 }
